7223029bce2a10390e83dea3ea87ea878becfe51
fill-p <83204197+fill-p@users.noreply.github.com>
2023-09-10 15:28:10 +0300

    add files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "";
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <span className="header_logo">logo</span>
+        </header>
+    )
+}
