15cc6f2ed44d5a1104e1d8e9ca7d0d431771af45
fill-p <83204197+fill-p@users.noreply.github.com>
2023-09-11 17:08:32 +0300

    add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+// import { ReactComponent as Icon} from "./language-icon1.png";
 import "./Header.css";
 
 export const Header = () => {
+  const [isOpen, setOpen] = useState();
   return (
-    <header className="header">
-      <span className="header_logo">logo</span>
-    </header>
+      <div className="header">
+      <span className="header_logo"><b>2KPDonate</b></span>
+      <nav className={`header_nav ${isOpen ? "active": ""} `}>
+        <ul className="header_nav_list">
+          <li className="header_nav_item">Цільові збори</li>
+          <li className="header_nav_item">Зробити внесок</li>
+          <li className="header_nav_item">Про нас</li>
+          <li className="header_nav_item">Сервіси</li>
+          <div className="header_language">
+            {/* <button className="header_button_language"><Icon /></button> */}
+            <button className="header_button_language">мова</button>
+          </div>
+        </ul>
+      </nav>
+      <button className="header_button_help">Допомоги</button>
+      <button className="header_button_profile">Профіль</button>
+      <button className="header_menu_button" onClick={() => setOpen(!isOpen)}></button>
+    </div>
   );
 };
