e4d066ac5d60dc06ac14be1f7a419ab6f697d086
KlyKir <klyauskirill@gmail.com>
2023-09-14 21:08:11 +0300

    Connected to server

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
+import "./App.css";
 import { Header } from "./components/Header/Header";
-
+//import { useEffect } from "react";
 function App() {
-    return(
-        <>
-            <Header />
-        </>
-    )
+  /*  const get = async () => {
+    const response = await fetch(
+      "http://localhost/2KPDonate/src/Server/getData.php"
+    );
+    const text = await response.text();
+    console.log(text);
+  };
+  useEffect(() => {
+    get();
+  }, []);*/
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
-export default App
+export default App;
